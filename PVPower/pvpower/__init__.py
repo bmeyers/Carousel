528dc7c2434063aa611a3b0c7851186fd9dda030
Bennet Meyers <bennetm@stanford.edu>
2016-10-24 23:51:11 -0700

    personalization

--- PVPower/pvpower/__init__.py
+++ PVPower/pvpower/__init__.py
@@ -1,13 +1,13 @@
 """
 This is the pvpower package.
-Tu
+Tutorial walk-through
 """
 
 import os
 
 __version__ = '0.1'
-__author__ = 'your name'
-__email__ = 'your.name@company.com'
+__author__ = 'bmeyers'
+__email__ = 'bennetm@stanford.edu'
 
 PKG_PATH = os.path.abspath(os.path.dirname(__file__))
 PROJ_PATH = os.path.dirname(PKG_PATH)
