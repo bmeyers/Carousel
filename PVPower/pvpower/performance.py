8465b1f16b31bbedf1be30ba2504d7cd8f70c695
Bennet Meyers <bennetm@stanford.edu>
2016-10-25 00:18:41 -0700

    specifying data

--- PVPower/pvpower/performance.py
+++ PVPower/pvpower/performance.py
@@ -5,6 +5,10 @@ PV power performance simulation
 from carousel.core.outputs import Output
 from carousel.core.calculations import Calc
 from carousel.core.formulas import Formula
+from carousel.core.data_sources import DataSource
+from carousel.core import UREG
+from datetime import datetime
+import pvlib
 
 
 class PVPowerOutputs(Output):
@@ -83,3 +87,51 @@ class UtilityFormulas(Formula):
             "units": ["=A", ["=A", None, None]]
         }
     }
+
+
+class PVPowerData(DataSource):
+    """
+    Data sources for PV Power demo.
+    """
+    latitude = {"units": "degrees", "uncertainty": 1.0}
+    longitude = {"units": "degrees", "uncertainty": 1.0}
+    elevation = {"units": "meters", "uncertainty": 1.0}
+    timestamp_start = {}
+    timestamp_count = {}
+    module = {}
+    inverter = {}
+    module_database = {}
+    inverter_database = {}
+    Tamb = {"units": "degC", "uncertainty": 1.0}
+    Uwind = {"units": "m/s", "uncertainty": 1.0}
+    surface_azimuth = {"units": "degrees", "uncertainty": 1.0}
+    timezone = {}
+
+    def __prepare_data__(self):
+        # set frequencies
+        for k in ('HOURLY', 'MONTHLY', 'YEARLY'):
+            self.data[k] = k
+            self.isconstant[k] = True
+        # apply metadata
+        for k, v in self.parameters.iteritems():
+            # TODO: this should be applied in data reader using _meta_names from
+            # data registry which should use a meta class and all parameter
+            # files should have same layout even xlrd and numpy readers, etc.
+            self.isconstant[k] = True  # set all data "isconstant" True
+            # uncertainty is dictionary
+            if 'uncertainty' in v:
+                self.uncertainty[k] = {k: v['uncertainty'] * UREG.percent}
+        # convert initial timestamp to datetime
+        self.data['timestamp_start'] = datetime(*self.data['timestamp_start'])
+        # get module and inverter databases
+        self.data['module_database'] = pvlib.pvsystem.retrieve_sam(
+            self.data['module_database'], path=SANDIA_MODULES
+        )
+        self.data['inverter_database'] = pvlib.pvsystem.retrieve_sam(
+            self.data['inverter_database'], path=CEC_INVERTERS
+        )
+        # get module and inverter
+        self.data['module'] = self.data['module_database'][self.data['module']]
+        self.data['inverter'] = (
+            self.data['inverter_database'][self.data['inverter']]
+        )
