c14dcef7d54c079b54b152c02da96e4151b5f27f
Bennet Meyers <bennetm@stanford.edu>
2016-10-24 23:30:41 -0700

    Starting the first tutorial

---
+++ PVPower/pvpower/__init__.py
@@ -0,0 +1,13 @@
+"""
+This is the pvpower package.
+Tu
+"""
+
+import os
+
+__version__ = '0.1'
+__author__ = 'your name'
+__email__ = 'your.name@company.com'
+
+PKG_PATH = os.path.abspath(os.path.dirname(__file__))
+PROJ_PATH = os.path.dirname(PKG_PATH)
